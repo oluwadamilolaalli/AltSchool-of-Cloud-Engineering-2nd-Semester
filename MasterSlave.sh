e0bb68d34dce0ed24162766f5990f6d7f9a615ef
oluwadamilolaalli <118380013+oluwadamilolaalli@users.noreply.github.com>
2023-10-28 16:38:53 +0100

    MasterSlave.sh

--- MasterSlave.sh
+++ MasterSlave.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-vagrant init ubuntu/bionic64
+vagrant init ubuntu-23.10.1-desktop-
 
 cat <<EOF > Vagrantfile
 Vagrant.configure("2") do |config|
 
-  config.vm.define "Master_1" do |Master_1|
+  config.vm.define "master" do |master|
 
-    Master_1.vm.hostname = "Master-1"
-    Master_1.vm.box = "ubuntu/bionic64"
-    Master_1.vm.network "private_network", ip: "192.168.20.10"
-    Master_1.vm.provision "shell", inline: <<-SHELL
+    master.vm.hostname = "Master-1"
+    master.vm.box = "ubuntu-23.10.1-desktop-"
+    master.vm.network "private_network", ip: "192.168.20.10"
+    master.vm.provision "shell", inline: <<-SHELL
     sudo apt-get update && sudo apt-get upgrade -y
     sudo apt-get-install -y avahi-daemon libnss-mdns
     SHELL
@@ -24,10 +24,12 @@ EOF
 
 # Define the "Slave" server configuration
 cat <<EOF >> Vagrantfile
-  config.vm.define "Slave_1" do |Slave-1|
-   Slave_1.vm.box = "ubuntu/bionic64"
-    Slave_1node.vm.network "private_network", ip: "192.168.20.11"
-    Slave_1.vm.provision "shell", inline: <<-SHELL
+  config.vm.define "slave" do |slave|
+
+    slave.vm.hostname = "slave-1"
+    slave.vm.box = "ubuntu-23.10.1-desktop-"
+    slave.vm.network "private_network", ip: "192.168.20.11"
+    slave.vm.provision "shell", inline: <<-SHELL
     sudo apt-get update && sudo apt-get upgrade -y
     sudo apt-get-install -y avahi-daemon libnss-mdns
     SHELL
@@ -41,8 +43,6 @@ EOF
 
 # Start and provision the servers
 vagrant up
-
-
 
 
 
