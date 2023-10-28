849c70cd05417939c6698018fda35c2ba34c1c9c
oluwadamilolaalli <118380013+oluwadamilolaalli@users.noreply.github.com>
2023-10-28 07:44:42 +0100

    Create MasterSlave.sh

---
+++ MasterSlave.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+vagrant init ubuntu/bionic64
+
+cat <<EOF > Vagrantfile
+Vagrant.configure("2") do |config|
+
+  config.vm.define "Master_1" do |Master_1|
+
+    Master_1.vm.hostname = "Master-1"
+    Master_1.vm.box = "ubuntu/bionic64"
+    Master_1.vm.network "private_network", ip: "192.168.20.10"
+    Master_1.vm.provision "shell", inline: <<-SHELL
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get-install -y avahi-daemon libnss-mdns
+    SHELL
+    node.vm.provider "virtualbox" do |vb|
+      vb.memory = 1024
+      vb.cpus = 2
+    end
+  end
+end
+EOF
+
+# Define the "Slave" server configuration
+cat <<EOF >> Vagrantfile
+  config.vm.define "Slave_1" do |Slave-1|
+   Slave_1.vm.box = "ubuntu/bionic64"
+    Slave_1node.vm.network "private_network", ip: "192.168.20.11"
+    Slave_1.vm.provision "shell", inline: <<-SHELL
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get-install -y avahi-daemon libnss-mdns
+    SHELL
+    config.vm.provider "virtualbox" do |vb|
+      vb.memory = 1024
+      vb.cpus = 2
+    end
+  end
+end
+EOF
+
+# Start and provision the servers
+vagrant up
+
+# SSH into the servers
+vagrant ssh Master_1
+source Bash-script.sh
+
+
+
+
